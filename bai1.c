19569908f22f027bc2fe7341b22069546d8b39ee
HUNGMANHPHI <106531155+HUNGMANHPHI@users.noreply.github.com>
2022-05-30 19:54:49 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,14 @@
+/* Fig. 2.1 : fig02_01.c
+   A first program in C */
+ #include <stdio.h>
+ /* functioon main begins progran excution */
+ int main (void) 
+{
+   printf ("Welvome to C !\n");
+   
+   return 0; /* indicate that program ended successfully */
+} /* end function main */
+
+
+
+	
